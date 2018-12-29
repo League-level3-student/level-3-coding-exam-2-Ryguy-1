f18e060b28dea69747adfe0f37e6f66196a5e4e0
Ryguy-1 <league@WTS5.attlocal.net>
2018-12-29 11:20:08 -0800

    done
    ez

--- src/test/HashMapCalculator.java
+++ src/test/HashMapCalculator.java
@@ -7,7 +7,6 @@ public class HashMapCalculator {
 
 	public ArrayList<String> commonKeyValues(ArrayList<Integer> lst, HashMap<Integer, String> hm) {
 		ArrayList<String> strings = new ArrayList<String>();
-
 			for (int j = 0; j < lst.size(); j++) {
 				int x = lst.get(j);
 			if(hm.containsKey(x)){
